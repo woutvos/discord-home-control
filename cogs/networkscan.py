c5cf9eb0c59849bbc72385dfa24b56ce0cc5d111
WoutVos <ikben@woutvos.nl>
2021-04-01 22:46:44 +0200

    Changed the Bot instance from client to bot

--- cogs/networkscan.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-
-class NetworkScan(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-def setup(bot):
-    bot.add_cog(NetworkScan(bot))
